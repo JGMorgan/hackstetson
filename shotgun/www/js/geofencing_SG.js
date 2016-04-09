8bc4bbc95b9ccb6ce325a444dd499a288e4d79e1
leonliang33 <llian008@fiu.edu>
2016-04-09 13:34:49 -0400

    geolocation, get longitude and latitude

--- shotgun/www/js/geofencing_SG.js
+++ shotgun/www/js/geofencing_SG.js
@@ -1,1 +1,26 @@
-// JavaScript source code
+
+// onSuccess Callback
+//   This method accepts a `Position` object, which contains
+//   the current GPS coordinates
+//
+function onSuccess(position) {
+    var element = document.getElementById('geolocation');
+    element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
+                        'Longitude: ' + position.coords.longitude + '<br />' +
+                        '<hr />' + element.innerHTML;
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: ' + error.code + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+// Options: throw an error if no update is received every 30 seconds.
+//
+
+function callLocation() {
+
+    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+}
