b8095b843eab4a163566c823e6bfc9f09b0f24bf
leonliang33 <llian008@fiu.edu>
2016-04-09 16:12:55 -0400

    firebase test

--- shotgun/www/js/firebaseDB_SG.js
+++ shotgun/www/js/firebaseDB_SG.js
@@ -1,2 +1,37 @@
 // JavaScript source code
 var Firebase = require("firebase");
+var newBase = new Firebase("https://shotgunapp.firebaseio.com/");
+//newBase.auth()
+function addUser() {
+
+}
+
+function getStatus(name) {
+    var n_stat = newBase.child(name);
+}
+
+var onComplete=function(error){
+    var element = document.getElementById('fbase');
+    if (error) {
+        console.log('Synchronization failed');
+    } else {
+        element.innerHTML = "changed to yes"
+    }
+}
+
+
+function determineSG(name) {
+    newBase.once("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            var key = childSnapshot.key();
+            var childData = childSnapshot.val();
+            if (childData == "yes") {
+                return true;
+            } else {
+                // update the name to value of true
+                newBase.update({name:'yes'},onComplete);
+                return true;
+            }
+        })
+    })
+}
