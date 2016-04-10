17d2d015d03e81b6e5f21d684fc86f230760670c
JGMorgan <josegm11194@gmail.com>
2016-04-09 23:01:23 -0400

    server works

--- shotgun/www/server.js
+++ shotgun/www/server.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var app = express();
-app.post('/',function(request,response){
-  console.log(request.body)
-});
 
-app.listen(6000,function(){
-  console.log("Started on PORT 6000");
-})
+app.route('/swag')
+
+  // show the form (GET http://localhost:1337/login)
+  .get(function(req,res) {
+    res.send('If you are seeing this something really bad went really wrong');
+  })
+
+  // process the form (POST http://localhost:1337/login)
+  .post(function(req, res) {
+    console.log('swag');
+    res.redirect('http://localhost:8100/')
+  });
+
+app.listen(1337);
+console.log('1337 is the dank port!');
